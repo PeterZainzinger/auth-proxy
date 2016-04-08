8d2b61c559a9b8e1addc5894f1e291c59fa16a1d
Peter Zainzinger <peterzainzinger@gmail.com>
2016-04-08 17:02:52 +0200

    make authentication optional

--- execute.sh
+++ execute.sh
@@ -1,6 +1,8 @@
 echo $CONFIG > data.json
 mustache data.json add-user.sh.mustache > add-user.sh
 mustache data.json nginx.conf.mustache > /etc/nginx/nginx.conf
+cat /etc/nginx/nginx.conf
+cat add-user.sh
 chmod +x add-user.sh
 ./add-user.sh
 nginx -g 'daemon off;'
