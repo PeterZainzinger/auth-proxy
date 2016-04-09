031326e258009e4e79ef34c159a66fd94bcb8b2d
Peter Zainzinger <peterzainzinger@gmail.com>
2016-04-09 20:54:28 +0200

    add possibility to add header

--- execute.sh
+++ execute.sh
@@ -2,7 +2,6 @@ echo $CONFIG > data.json
 mustache data.json add-user.sh.mustache > add-user.sh
 mustache data.json nginx.conf.mustache > /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
-cat add-user.sh
 chmod +x add-user.sh
 ./add-user.sh
 nginx -g 'daemon off;'
